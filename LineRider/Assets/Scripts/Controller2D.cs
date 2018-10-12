1b5a4b867b067a72fc258d5ea323cffb98a31ac8
grossjw58 <44102520+grossjw58@users.noreply.github.com>
2018-10-12 18:14:39 -0400

    PlayerMovementE02
    Player movement updated though EO2

--- LineRider/Assets/Scripts/Controller2D.cs
+++ LineRider/Assets/Scripts/Controller2D.cs
@@ -6,6 +6,8 @@ using UnityEngine;
 public class Controller2D : MonoBehaviour {
 
 	//seralized global private variables to be edited in the inspector
+	[Tooltip("layers to include for collision detection of 2Dcontroller")]
+	[SerializeField] LayerMask collisionMask;
 	[Tooltip("The number of rays to send out in the horizontal direction (front and back)")]
 	[Range(2,300)]
 	[SerializeField] int horizontalRayCount=4;
@@ -27,25 +29,27 @@ public class Controller2D : MonoBehaviour {
 	void Start () {
 		collider=GetComponent<BoxCollider2D>();//get the collider from the gameobject
 		bounds = collider.bounds;//update the bounds of the object
-	}
-	// Update is called once a frame
-	void Update(){
-		UpdateBounds();
-		UpdateRaycastOrigins();
+		bounds.Expand(skinWidth*-2);
 		CalculateRaySpacing();
-		//For testing purposes will remove later 
-		for (int i=0; i<verticalRayCount;i++){
-			Debug.DrawRay(raycastOrigins.bottomLeft + Vector2.right*verticalRaySpacing*i,Vector2.up*-2,Color.red);
-		}
 	}
 
-	void UpdateBounds(){
-		//updates the bounds based on the collider to make sure it is current with object movement and any time the collider changes size
-		bounds = collider.bounds;
-		bounds.Expand(skinWidth*-2);
+
+	public void Move(Vector3 _velocity){
+		UpdateRaycastOrigins();
+		if(_velocity.x!=0){
+			HorizontalCollisions(ref _velocity);
+		}
+		if(_velocity.y!=0){
+			VerticalCollisions(ref _velocity);
+		}
+		transform.Translate(_velocity);
 	}
 
 	void UpdateRaycastOrigins(){
+		//update bounds then update the current positions of the corners of the box collider based on those bounds
+		//update the bounds of the object
+		bounds = collider.bounds;
+		bounds.Expand(skinWidth*-2);
 		//update the current positions of the corners of the box collider based on the bounds
 		raycastOrigins.bottomLeft=new Vector2(bounds.min.x,bounds.min.y);
 		raycastOrigins.bottomRight=new Vector2(bounds.max.x,bounds.min.y);
@@ -57,6 +61,40 @@ public class Controller2D : MonoBehaviour {
 		//calcualte the horizontal and vertical spacing between raycasts 
 		horizontalRaySpacing=bounds.size.y/(horizontalRayCount-1);
 		verticalRaySpacing=bounds.size.x/(verticalRayCount-1);
+	}
+
+	void VerticalCollisions(ref Vector3 _velocity){
+		//detect verticle collisions (top and bottom) and adjust player position accordingly 
+		float directionY=Mathf.Sign(_velocity.y);//get the direction the player is moving vertically
+		float rayLength=Mathf.Abs(_velocity.y+skinWidth);//set the length of the collision dectection ray equal to the distance the player wants to move this frame
+		//loop through each of the raycast origins and cast a ray to check for colliions 
+		for (int i=0; i<verticalRayCount;i++){
+			Vector2 rayOrigin = (directionY==-1)?raycastOrigins.bottomLeft:raycastOrigins.topLeft;//determine which end (top vs bottom) to detect collisions from 
+			rayOrigin+=Vector2.right*(verticalRaySpacing*i+_velocity.x);
+			RaycastHit2D hit=Physics2D.Raycast(rayOrigin,Vector2.up*directionY,rayLength,collisionMask);
+			Debug.DrawRay(rayOrigin,Vector2.up*directionY*rayLength,Color.red);//debug statement may remove
+			//if a raycast hits anything change the movement distance accordinly and shorten the raylength to match that distance 
+			if(hit){
+				_velocity.y=(hit.distance-skinWidth)*directionY;
+				rayLength=hit.distance;
+			}
+		}
+	}
+
+	void HorizontalCollisions(ref Vector3 _velocity){
+		//detect horizontal collisions (left and right) and adjust player move distance accordingly for detailed comments see VerticalCollisions method
+		float directionX=Mathf.Sign(_velocity.x);
+		float rayLength=Mathf.Abs(_velocity.x+skinWidth);
+		for (int i=0; i<horizontalRayCount;i++){
+			Vector2 rayOrigin = (directionX==-1)?raycastOrigins.bottomLeft:raycastOrigins.bottomRight;
+			rayOrigin+=Vector2.up*(horizontalRaySpacing*i);
+			RaycastHit2D hit=Physics2D.Raycast(rayOrigin,Vector2.right*directionX,rayLength,collisionMask);
+			Debug.DrawRay(rayOrigin,Vector2.right*directionX*rayLength,Color.red);
+			if(hit){
+				_velocity.x=(hit.distance-skinWidth)*directionX;
+				rayLength=hit.distance;
+			}
+		}
 	}
 
 	struct RaycastOrigins{
--- LineRider/Assets/Scripts/PlayerMotor.cs
+++ LineRider/Assets/Scripts/PlayerMotor.cs
@@ -3,11 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 [RequireComponent(typeof(Controller2D))]
 public class PlayerMotor : MonoBehaviour {
+	//seralized global private variables to be edited in the inspector
+	[Tooltip("The gravity to be used on the player meaured in world units per second")]
+	[SerializeField]float gravity=-20f;
+	[Tooltip("The horizontal speed at which you want the player to move measured in world units per second")]
+	[SerializeField]float moveSpeed=6f;
 	//global private variables
 	Controller2D controller;//a refernce to the Controller2D script attached to the game object
+	Vector3 velocity;//a vector3 to hold how far the player will move this frame
+
 
 	// Use this for initialization
 	void Start(){
 		controller = GetComponent<Controller2D>();//get the Controller2D from the gameobject
+	}
+
+	// Update is called once a frame
+	void Update(){
+
+		Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"),Input.GetAxisRaw("Vertical"));
+
+		velocity.x=input.x*moveSpeed;
+		velocity.y+=gravity*Time.deltaTime;
+		controller.Move(velocity*Time.deltaTime);
 	}
 }
